1d6047679120d34146b3786765cb9c30f86a9220
har1nagle <hnagle.0@yahoo.com>
2024-08-31 11:19:51 +0530

    varificationToken issue solved and postman via mongoDb stored

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -12,21 +14,24 @@ export const signup = async (req, res) => {
     const userAlreadyExists = await User.findOne({ email })
     
     console.log("userAlreadyExists", userAlreadyExists);
+
     if (userAlreadyExists) {
       return res.status(400).json({success:false, message: "User already exists" });
     }
 
     const hashedPassword = await bcryptjs.hash(password, 10);
-    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+    const varificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
 
 
     const user = new User({
       email,
       password: hashedPassword,
       name,
-      verificationToken,
-      varificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 hours
-    })
+      varificationToken,
+      varificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    });
 
     await user.save();
 
--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -9,8 +9,9 @@ export const generateTokenAndSetCookie = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    // maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 3600000,
   });
 
-  return token
-}
+  return token;
+};
