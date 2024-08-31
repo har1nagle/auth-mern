e4aacfa9565a1af73b315ac965bcfd06ec7cd679
har1nagle <hnagle.0@yahoo.com>
2024-08-31 20:52:19 +0530

    signup is done here

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -3,11 +3,12 @@ import { mailtrapClient, sender } from "./mailtrap.config.js";
 
  
 export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipient = [{email}]
 
   try {
     const response = await mailtrapClient.send({
       from: sender,
-      to: [{ email }],
+      to: recipient,
       subject: "Verify your email",
       html: VERIFICATION_EMAIL_TEMPLATE.replace(
         "{verificationCode}",
