539a06b85d8e288b046d6f3808d47ce29300f487
har1nagle <hnagle.0@yahoo.com>
2024-09-05 23:32:31 +0530

    Components done PasswordStrenthMeter

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,8 @@ import { motion } from "framer-motion"
 import Input from "../components/Input"
 import {Mail, User, Lock} from "lucide-react"
 import { useState } from "react"
+import { Link } from "react-router-dom"
+import PasswordStrenthMeter from "../components/PasswordStrenthMeter"
 
 const SignUpPage = () => {
 
@@ -47,6 +49,8 @@ const SignUpPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <PasswordStrenthMeter password={password}/>
+
           <motion.button
             className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg
           shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
@@ -60,6 +64,15 @@ const SignUpPage = () => {
             Sign Up
           </motion.button>
         </form>
+      </div>
+
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Already have an account ? {""}
+          <Link to={"/login"} className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
       </div>
     </motion.div>
   );
